b373240eaf4cbc129d84772e64b2eb4b270de520
ghada <ghada.tarik.fcis@gmail.com>
2019-09-24 05:21:31 +0200

    intial project

---
+++ app/src/main/java/com/example/routegit/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.routegit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void on_Android_Click(View view)
+    {
+
+    }
+    public void on_Ios_Click(View view)
+    {
+
+    }
+    public void on_Full_Stack_Click(View view)
+    {
+
+    }
+}
---
+++ app/src/main/java/com/example/routegit/splashscreen.java
@@ -0,0 +1,26 @@
+package com.example.routegit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashscreen extends AppCompatActivity {
+   private static int SPLASH_TIME_OUT=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent =new Intent(splashscreen.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
